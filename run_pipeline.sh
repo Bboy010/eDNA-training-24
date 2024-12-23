2e8f74263c2399adadadab21c1d4c69d72a3834d
Hongo <104635374+Bboy010@users.noreply.github.com>
2024-12-23 03:12:19 -1200

    Update run_pipeline.sh

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-# Activer l'environnement Conda
-conda activate eDNA-training-24
-
-# Lancer le pipeline
 nextflow run nf-core/demo -r 1.0.1 \
     -profile conda \
     -c config/custom.config \
     --input input_data/samplesheet.csv \
     --outdir results/demo \
     -resume
-
